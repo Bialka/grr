79c6477ca786c645b08e8602ab8f8a8e40a6472d
Cosiek <fitek2@o2.pl>
2016-04-10 18:06:20 +0200

    Add some initial files for horse subapp

---
+++ grr/grr/horse/races_initial_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# agility = 'zwinność'
+# obedience = 'posłuszeństwo'
+# stamina = 'wytrzymałość'
+# strength = 'siła'
+# speed = 'szybkość'
+
+races_data = {
+    'nazwa': {
+        'agility': 0,
+        'max_agility': 0,
+        'obedience': 0,
+        'max_obedience': 0,
+        'stamina': 0,
+        'max_stamina': 0,
+        'strength': 0,
+        'max_strength': 0,
+        'speed': 0,
+        'max_speed': 0,
+    },
+}
